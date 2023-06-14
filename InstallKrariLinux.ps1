aa7ca2724a5b73ab8eda03501c9321fb5d27261c
LucasMailly <lucas.mailly@proton.me>
2023-06-14 21:35:06 +0200

    Fix.

--- InstallKrariLinux.ps1
+++ InstallKrariLinux.ps1
@@ -1,13 +1,14 @@
 # Download the background image from our github
-$filePath = "~\images"
+$filePath = "~\Pictures\krarilinux-wallpaper.png"
 wget -O "$filepath" "https://raw.githubusercontent.com/krari-linux/krari-linux/main/krarilinux-wallpaper.png"
 # Set the image as a background
+$file = (Get-Item $filePath).FullName
 $signature = @"
 [DllImport("user32.dll", CharSet = CharSet.Auto)]
 public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 "@
-$systemParametersInfo = Add-Type -MemberDefinition $signature -Name Wallpaper -Namespace WinAPI -PassThru
-$null = $systemParametersInfo::SystemParametersInfo(20, 0, $filePath, 3)
+$systemParametersInfo = Add-Type -MemberDefinition $signature -Name Win32Utils -Namespace SystemParametersInfo -PassThru
+$null = $systemParametersInfo::SystemParametersInfo(20, 0, $file, 3)
 
 # Hide icons on your desktop (like a real hacker) by setting the registry path and value name
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
@@ -18,6 +19,8 @@ if (!(Test-Path $registryPath)) {
 }
 # Hide the icons by setting the registry value to 1
 Set-ItemProperty -Path $registryPath -Name $valueName -Value 1
+# Restart the explorer.exe process to apply the changes
+Stop-Process -Name explorer -Force
 
 #Open your browser to start hacking
 $url = "https://geektyper.com/fsociety/"
